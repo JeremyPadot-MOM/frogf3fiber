d1609c14e3fb3b9a3457ccda2eecad192d09e0a4
Jeremy Kale Padot <jeremy@padot.us>
2020-05-08 14:11:33 -0700

    install yarn, eslint error

--- src/index.js
+++ src/index.js
@@ -2,10 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Canvas } from "react-three-fiber";
 
-import "./styles.css";
+import "./app.css";
+
+function Light({brightness, color}) {
+  return (
+    <reactAreaLight
+         width={3}
+      height={3}
+      color={color}
+      intensity={brightness}
+      position={[-2, 0, 5]}
+      lookAt={[0, 0, 0]}
+      penumbra={1}
+      castShadow
+    />
+  );
+}
+
+function Sphere() {
+  return (
+    <mesh visible userData={{ test: "hello" }} position={[0, 0, 0]} castShadow>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        roughness={0.1}
+        metalness={0.1}
+      />
+    </mesh>
+  );
+}
 
 function App() {
-  return <Canvas />;
+  return (
+    <Canvas>
+      <Sphere />
+    </Canvas>
+  );
 }
 
 const rootElement = document.getElementById("root");
