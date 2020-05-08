3c9583c01c2fbe1ceb8749d2104573ff47eb6bdf
Jeremy Kale Padot <jeremy@padot.us>
2020-05-08 15:57:05 -0700

    add 2 and 3 point lighting to sphere

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 import { Canvas } from "react-three-fiber";
 
 import "./App.css";
 
-function KeyLight({ brightness, color }) {
-  return (
-    <rectAreaLight
-      width={3}
-      height={3}
-      color={color}
-      intensity={brightness}
-      position={[-2, 0, 5]}
-      lookAt={[0, 0, 0]}
-      penumbra={1}
-      castShadow
-    />
-  );
-}
 // Geometry
 function GroundPlane() {
   return (
@@ -35,7 +21,6 @@ function BackDrop() {
     </mesh>
   );
 }
-
 function Sphere() {
   return (
     <mesh
@@ -56,14 +41,58 @@ function Sphere() {
     </mesh>
   );
 }
+// Lights
+function KeyLight({ brightness, color }) {
+  return (
+    <rectAreaLight
+      width={3}
+      height={3}
+      color={color}
+      intensity={brightness}
+      position={[-2, 0, 5]}
+      lookAt={[0, 0, 0]}
+      penumbra={1}
+      castShadow
+    />
+  );
+}
+function FillLight({ brightness, color }) {
+  return (
+    <rectAreaLight
+      width={3}
+      height={3}
+      intensity={brightness}
+      color={color}
+      position={[2, 1, 4]}
+      lookAt={[0, 0, 0]}
+      penumbra={2}
+      castShadow
+    />
+  );
+}
+function RimLight({ brightness, color }) {
+  return (
+    <rectAreaLight
+      width={2}
+      height={2}
+      intensity={brightness}
+      color={color}
+      position={[1, 4, -2]}
+      rotation={[0, 180, 0]}
+      castShadow
+    />
+  );
+}
 
 function App() {
   return (
-    <Canvas>
-      <KeyLight brightness={10} color={"white"} />
-      <Sphere />
-      <BackDrop />
+    <Canvas className="canvas">
       <GroundPlane />
+      <BackDrop />
+      <KeyLight brightness={5.6} color="#ffbdf4" />
+      <FillLight brightness={2.6} color="#bdefff" />
+      <RimLight brightness={54} color="#fff" />
+      <Sphere />
     </Canvas>
   );
 }
