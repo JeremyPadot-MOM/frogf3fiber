9070fe7a137ac1620b16a9f8f6175ba96b254417
Jeremy Kale Padot <jeremy@padot.us>
2020-05-18 11:21:33 -0700

    model not showing,

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useRef } from "react";
+import { Canvas, useLoader, useFrame } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import "./App.css";
 
-function App() {
+function Loading() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
   );
 }
 
-export default App;
+function F3f() {
+  const group = useRef();
+  const { nodes } = useLoader(GLTFLoader, "models/f3f1.glb");
+  useFrame(() => {
+    group.current.rotation.y += 0.004;
+    
+  });
+  return (
+    <group ref={group}>
+      <mesh visible geometry={nodes.Default.geometry}>
+        <meshStandardMaterial
+          attach="material"
+          color="white"
+          roughness={0.3}
+          metalness={0.3}
+        />
+      </mesh>
+    </group>
+  );
+}
+
+
+export default function App() {
+  return (
+    
+      <Canvas style={{ background: "#171717" }}>
+        <directionalLight intensity={0.5} />
+        <Suspense fallback={<Loading />}>
+          <F3f />
+        </Suspense>
+      </Canvas>
+      );
+      }
--- src/index.js
+++ src/index.js
@@ -104,10 +104,66 @@
 // watching blender tutorial on character modeling - plan to import character in place of primitive shape
 // drawing front and side prototypes for character model 
 // finished illustrations of proto character models, front and side looking right
-import React from "react";
-import { Canvas } from "react-three-fiber";
-import "./App.css";
+// import React, { Suspense, useRef } from "react";
+// import { Canvas, useLoader, useFrame } from "react-three-fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import "./App.css";
 
-export default function App()
-return <Canvas style={{ background : "#171717" }}></Canvas>;
-}
+// function Loading() {
+//   return (
+//     <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+//       <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+//       <meshStandardMaterial
+//         attach="material"
+//         color="white"
+//         transparent
+//         opacity={0.6}
+//         roughness={1}
+//         metalness={0}
+//       />
+//     </mesh>
+//   );
+// }
+
+// function F3f() {
+//   const group = useRef();
+//   const { nodes } = useLoader(GLTFLoader, "models/f3f1.glb");
+//   useFrame(() => {
+//     group.current.rotation.y += 0.004;
+//   });
+//   return (
+//     <group ref={group}>
+//       <mesh visible geometry={nodes.Default.geometry}>
+//         <meshStandardMaterial
+//           attach="material"
+//           color="white"
+//           roughness={0.3}
+//           metalness={0.3}
+//         />
+//       </mesh>
+//     </group>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <Canvas style={{ background: "#171717" }}>
+//       <directionalLight intensity={0.5} />
+//       <Suspense fallback={<Loading />}>
+//         <F3f />
+//       </Suspense>
+//     </Canvas>
+//   );
+// }
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
