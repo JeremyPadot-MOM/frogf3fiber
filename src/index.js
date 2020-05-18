f10d66b448a52e2370d095036a128459cf94d9d5
Jeremy Kale Padot <jeremy@padot.us>
2020-05-18 10:47:12 -0700

    start a new blank app with empty canvas, here we go

--- src/index.js
+++ src/index.js
@@ -1,106 +1,113 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Canvas } from "react-three-fiber";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Canvas } from "react-three-fiber";
 
-import "./App.css";
+// import "./App.css";
 
-// Geometry
-function GroundPlane() {
-  return (
-    <mesh receiveShadow rotation={[5, 0, 0]} position={[0, -1, 0]}>
-      <planeBufferGeometry attach="geometry" args={[500, 500]} />
-      <meshStandardMaterial attach="material" color="white" />
-    </mesh>
-  );
-}
-function BackDrop() {
-  return (
-    <mesh receiveShadow position={[0, -1, -5]}>
-      <planeBufferGeometry attach="geometry" args={[500, 500]} />
-      <meshStandardMaterial attach="material" color="white" />
-    </mesh>
-  );
-}
-function Sphere() {
-  return (
-    <mesh
-      visible
-      userData={{ test: "hello" }}
-      position={[0, 0, 0]}
-      rotation={[0, 0, 0]}
-      castShadow
-    >
-      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
-      <meshStandardMaterial
-        attach="material"
-        color="white"
-        transparent
-        roughness={0.1}
-        metalness={0.1}
-      />
-    </mesh>
-  );
-}
+// // Geometry
+// function GroundPlane() {
+//   return (
+//     <mesh receiveShadow rotation={[5, 0, 0]} position={[0, -1, 0]}>
+//       <planeBufferGeometry attach="geometry" args={[500, 500]} />
+//       <meshStandardMaterial attach="material" color="white" />
+//     </mesh>
+//   );
+// }
+// function BackDrop() {
+//   return (
+//     <mesh receiveShadow position={[0, -1, -5]}>
+//       <planeBufferGeometry attach="geometry" args={[500, 500]} />
+//       <meshStandardMaterial attach="material" color="white" />
+//     </mesh>
+//   );
+// }
+// function Sphere() {
+//   return (
+//     <mesh
+//       visible
+//       userData={{ test: "hello" }}
+//       position={[0, 0, 0]}
+//       rotation={[0, 0, 0]}
+//       castShadow
+//     >
+//       <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+//       <meshStandardMaterial
+//         attach="material"
+//         color="white"
+//         transparent
+//         roughness={0.1}
+//         metalness={0.1}
+//       />
+//     </mesh>
+//   );
+// }
 
-// Lights
-function KeyLight({ brightness, color }) {
-  return (
-    <rectAreaLight
-      width={3}
-      height={3}
-      color={color}
-      intensity={brightness}
-      position={[-2, 0, 5]}
-      lookAt={[0, 0, 0]}
-      penumbra={1}
-      castShadow
-    />
-  );
-}
-function FillLight({ brightness, color }) {
-  return (
-    <rectAreaLight
-      width={3}
-      height={3}
-      intensity={brightness}
-      color={color}
-      position={[2, 1, 4]}
-      lookAt={[0, 0, 0]}
-      penumbra={2}
-      castShadow
-    />
-  );
-}
+// // Lights
+// function KeyLight({ brightness, color }) {
+//   return (
+//     <rectAreaLight
+//       width={3}
+//       height={3}
+//       color={color}
+//       intensity={brightness}
+//       position={[-2, 0, 5]}
+//       lookAt={[0, 0, 0]}
+//       penumbra={1}
+//       castShadow
+//     />
+//   );
+// }
+// function FillLight({ brightness, color }) {
+//   return (
+//     <rectAreaLight
+//       width={3}
+//       height={3}
+//       intensity={brightness}
+//       color={color}
+//       position={[2, 1, 4]}
+//       lookAt={[0, 0, 0]}
+//       penumbra={2}
+//       castShadow
+//     />
+//   );
+// }
 
-function RimLight({ brightness, color }) {
-  return (
-    <rectAreaLight
-      width={2}
-      height={2}
-      intensity={brightness}
-      color={color}
-      position={[1, 4, -2]}
-      rotation={[0, 180, 0]}
-      castShadow
-    />
-  );
-}
-function App() {
-  return (
-    <Canvas className="canvas">
-      <GroundPlane />
-      <BackDrop />
-      <KeyLight brightness={5.6} color={"#ffbdf4"} />
-      <FillLight brightness={2.6} color={"#bdefff"} />
-      <RimLight brightness={54} color={"#fff"} />
-      <Sphere />
-    </Canvas>
-  );
-}
+// function RimLight({ brightness, color }) {
+//   return (
+//     <rectAreaLight
+//       width={2}
+//       height={2}
+//       intensity={brightness}
+//       color={color}
+//       position={[1, 4, -2]}
+//       rotation={[0, 180, 0]}
+//       castShadow
+//     />
+//   );
+// }
+// function App() {
+//   return (
+//     <Canvas className="canvas">
+//       <GroundPlane />
+//       <BackDrop />
+//       <KeyLight brightness={5.6} color={"#ffbdf4"} />
+//       <FillLight brightness={2.6} color={"#bdefff"} />
+//       <RimLight brightness={54} color={"#fff"} />
+//       <Sphere />
+//     </Canvas>
+//   );
+// }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
 
 // watching blender tutorial on character modeling - plan to import character in place of primitive shape
 // drawing front and side prototypes for character model 
 // finished illustrations of proto character models, front and side looking right
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import "./App.css";
+
+export default function App()
+return <Canvas style={{ background : "#171717" }}></Canvas>;
+}
