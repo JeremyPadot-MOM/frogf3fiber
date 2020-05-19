d43fe680adb85861a321abc47578edff9f0dbbd1
Jeremy Kale Padot <jeremy@padot.us>
2020-05-19 08:30:26 -0700

    good morning, lets figure out some bugs shall we

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 function Loading() {
   return (
-    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+    // <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
       <sphereGeometry attach="geometry" args={[1, 16, 16]} />
       <meshStandardMaterial
         attach="material"
@@ -15,19 +15,19 @@ function Loading() {
         roughness={1}
         metalness={0}
       />
-    </mesh>
+    // </mesh>
   );
 }
 
 function F3f() {
   const group = useRef();
-  //working below
+  //testing below doesnt seem to change much, rotates faster?
 useFrame(state => {
     group.current.position.y = ((1 + Math.sin(state.clock.getElapsedTime())) / 2) * 2
     group.current.rotation.y += 0.01
   })
 
-  //working above
+  //testing above
   const { nodes } = useLoader(GLTFLoader, "models/f3f1.glb");
   console.log(nodes);
   useFrame(() => {
@@ -56,6 +56,7 @@ export default function App() {
   return (
     
       <Canvas style={{ background: "#171717" }}>
+      {/* camera? */}
         <directionalLight intensity={0.5} />
         <Suspense fallback={<Loading />}>
           <F3f />
--- src/index.js
+++ src/index.js
@@ -163,6 +163,7 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
