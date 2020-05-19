71d883db1fc6f53e7693a479e1388afed04998e2
Jeremy Kale Padot <jeremy@padot.us>
2020-05-19 09:22:33 -0700

    use Three hook to reference the threejs camera and canvas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Suspense, useRef } from "react";
-import { Canvas, useLoader, useFrame } from "react-three-fiber";
+import { Canvas, useLoader, useFrame, extend, useThree, } from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { softShadows } from "drei"
 import "./App.css";
 
+extend({ OrbitControls });
+
 softShadows({ size: 0.005, frustrum: 2.75 })
 
 function Loading() {
   return (
-    // <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
       <sphereGeometry attach="geometry" args={[1, 16, 16]} />
       <meshStandardMaterial
         attach="material"
@@ -19,18 +21,17 @@ function Loading() {
         roughness={1}
         metalness={0}
       />
-    // </mesh>
+     </mesh>
   );
 }
 
 function F3f() {
   const group = useRef();
   //testing below doesnt seem to change much, rotates faster?
-useFrame(state => {
-    group.current.position.y = ((1 + Math.sin(state.clock.getElapsedTime())) / 2) * 2
-    group.current.rotation.y += 0.01
-  })
-
+// useFrame(state => {
+//     group.current.position.y = ((1 + Math.sin(state.clock.getElapsedTime())) / 2) * 2
+//     group.current.rotation.y += 0.01
+//   })
   //testing above
   const { nodes } = useLoader(GLTFLoader, "models/f3f1.glb");
   console.log(nodes);
@@ -51,16 +52,24 @@ useFrame(state => {
         
       </mesh>
     </group>
-  )
+  );
 }
-
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  const controls = useRef();
+  useFrame((state) => controls.current.update());
+  return <orbitControls ref={controls} args={[camera, domElement]} />;
+};
 
 
 export default function App() {
   return (
     
-      <Canvas style={{ background: "#171717" }}>
-      {/* camera? */}
+      <Canvas style={{ background: "white" }}>
+      <CameraControls />
         <directionalLight intensity={0.5} />
         <Suspense fallback={<Loading />}>
           <F3f />
